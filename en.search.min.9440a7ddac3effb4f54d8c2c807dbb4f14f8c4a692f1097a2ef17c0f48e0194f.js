cbb789d1fe4565452328a5a411aa1c8c032d6259
dwmkerr <dwmkerr@users.noreply.github.com>
2020-01-05 09:36:35 +0000

    Automated deployment: Sun Jan  5 09:36:35 UTC 2020 dfc160cf1d07e777cd5ac0a2f8367962b7289738

---
+++ en.search.min.9440a7ddac3effb4f54d8c2c807dbb4f14f8c4a692f1097a2ef17c0f48e0194f.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.2e758c02e0e0855e532020be16c8148e44450ca6db75acea06c793b6686939a4.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
