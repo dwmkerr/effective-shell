b7167b27e401834b74b5166d06a8928054580763
Dave Kerr <dwmkerr@gmail.com>
2024-05-14 10:06:34 +1000

    fix: correct artifacts path

--- scripts/build-samples.sh
+++ scripts/build-samples.sh
@@ -43,5 +43,5 @@ mkdir -p ./artifacts/samples/repositories
 )
 
 # Zip up and tar up the samples
-(cd ../artifacts && zip -r ./samples.zip ./samples)
+(cd ./artifacts && zip -r ./samples.zip ./samples)
 tar czf ./artifacts/samples.tar.gz -C ./artifacts/samples .
