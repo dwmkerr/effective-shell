167a6d384552ce73f49e4a738166cf87f5e1e7b4
Dave Kerr <dwmkerr@gmail.com>
2025-01-27 15:15:34 +0000

    fix: showpstree script errors if pstree not installed and shows how to install it (#362)

--- samples/scripts/showpstree.sh
+++ samples/scripts/showpstree.sh
@@ -3,10 +3,27 @@
 # showpstree - Shows the process tree for the current process.
 
 if [ "$(uname)" = "Darwin" ]; then
-    # BSD pstree; show for process -p.
-    pstree -p $$
+    # Check whether pstree is installed.
+    if ! command -v pstree 2>&1 >/dev/null; then
+        echo "error: pstree not installed, try:"
+        echo "  brew install pstree"
+        exit 1
+    else
+        # BSD pstree; show for process -p.
+        pstree -p $$
+    fi
 else
-    # GNU pstree; the command line (-a) and the
-    # details for a specific process (-s).
-    pstree -a -s $$
+    # Check whether pstree is installed.
+    if ! command -v pstree 2>&1 >/dev/null; then
+        echo "error: pstree not installed, try:"
+        echo "  yum install psmisc"
+        echo "or:"
+        echo "  apt-get install psmisc"
+
+        exit 1
+    else
+        # GNU pstree; the command line (-a) and the
+        # details for a specific process (-s).
+        pstree -a -s $$
+    fi
 fi
